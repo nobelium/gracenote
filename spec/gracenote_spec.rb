a48abd91758ad8ac38f2a0d2f4f11755abe1f403
Nobelium <me@vignesh.info>
2014-04-16 22:59:57 +0000

    Tests - starting to work

--- spec/gracenote_spec.rb
+++ spec/gracenote_spec.rb
@@ -2,50 +2,48 @@ require "spec_helper"
 
 describe Gracenote do 
 
-  before :all do
-    @obj = Gracenote.new({:clientID => "7097600", :clientTag => "35312F0A797B9FE6F24AC32CDE64AC5B"})
-  end
+  let(:gn) { Gracenote.new({:clientID => "7097600", :clientTag => "35312F0A797B9FE6F24AC32CDE64AC5B"}) }
 
   describe "Configuration" do
     it "should allow to read clientID" do
-      id = @obj.clientID
+      id = obj.clientID
       id.should == "7097600"
     end
 
     it "should allow to read clientTag" do
-      tag = @obj.clientTag
+      tag = gn.clientTag
       tag.should == "35312F0A797B9FE6F24AC32CDE64AC5B"
     end
 
     it "should allow to read api url" do
-      url = @obj.apiURL
-      url.should == "https://c" + @obj.clientID + ".web.cddbp.net/webapi/xml/1.0/"
+      url = gn.apiURL
+      url.should == "https://c" + gn.clientID + ".web.cddbp.net/webapi/xml/1.0/"
     end
 
     it "should allow to read userid" do
-      userid = @obj.userID
+      userid = gn.userID
       userid.should == nil
     end
 
     it "allows to set clientID" do
-      clientID = @obj.clientID
-      @obj.clientID = "1234567"
-      @obj.clientID.should == "1234567"
-      @obj.clientID = clientID
+      clientID = gn.clientID
+      gn.clientID = "1234567"
+      gn.clientID.should == "1234567"
+      gn.clientID = clientID
     end
 
     it "allows to set clientTag" do
-      clientTag = @obj.clientTag
-      @obj.clientTag = "1234567"
-      @obj.clientTag.should == "1234567"
-      @obj.clientTag = clientTag
+      clientTag = gn.clientTag
+      gn.clientTag = "1234567"
+      gn.clientTag.should == "1234567"
+      gn.clientTag = clientTag
     end
 
     it "allows to set api url" do 
-      apiurl = @obj.apiURL
-      @obj.apiURL = "http://asdf.com"
-      @obj.apiURL.should == "http://asdf.com"
-      @obj.apiURL = apiurl
+      apiurl = gn.apiURL
+      gn.apiURL = "http://asdf.com"
+      gn.apiURL.should == "http://asdf.com"
+      gn.apiURL = apiurl
     end
 
   end
