2c84a54ebfdae394649b4e605728334b9125da08
geun <geunbaelee@gmail.com>
2016-06-27 01:11:26 +0900

    ruby code convention

--- spec/lib/gracenote_config_spec.rb
+++ spec/lib/gracenote_config_spec.rb
@@ -2,48 +2,48 @@ require "spec_helper"
 
 describe Gracenote do 
 
-  let(:gn) { Gracenote.new({:clientID => "7097600", :clientTag => "35312F0A797B9FE6F24AC32CDE64AC5B"}) }
+  let(:gn) { Gracenote.new({:client_id => "7097600", :client_tag => "35312F0A797B9FE6F24AC32CDE64AC5B"}) }
 
   describe "Configuration" do
-    it "should allow to read clientID" do
-      id = gn.clientID
+    it "should allow to read @client_id" do
+      id = gn.client_id
       id.should == "7097600"
     end
 
-    it "should allow to read clientTag" do
-      tag = gn.clientTag
+    it "should allow to read @client_tag" do
+      tag = gn.client_tag
       tag.should == "35312F0A797B9FE6F24AC32CDE64AC5B"
     end
 
     it "should allow to read api url" do
-      url = gn.apiURL
-      url.should == "https://c" + gn.clientID + ".web.cddbp.net/webapi/xml/1.0/"
+      url = gn.api_endpoint_url
+      url.should == "https://c" + gn.client_id + ".web.cddbp.net/webapi/xml/1.0/"
     end
 
     it "should allow to read userid" do
-      userid = gn.userID
+      userid = gn.user_id
       userid.should == nil
     end
 
-    it "allows to set clientID" do
-      clientID = gn.clientID
-      gn.clientID = "1234567"
-      gn.clientID.should == "1234567"
-      gn.clientID = clientID
+    it "allows to set @client_id" do
+      client_id = gn.client_id
+      gn.client_id = "1234567"
+      gn.client_id.should == "1234567"
+      gn.client_id = client_id
     end
 
-    it "allows to set clientTag" do
-      clientTag = gn.clientTag
-      gn.clientTag = "1234567"
-      gn.clientTag.should == "1234567"
-      gn.clientTag = clientTag
+    it "allows to set @client_tag" do
+      client_tag = gn.client_tag
+      gn.client_tag = "1234567"
+      gn.client_tag.should == "1234567"
+      gn.client_tag = client_tag
     end
 
     it "allows to set api url" do 
-      apiurl = gn.apiURL
-      gn.apiURL = "http://asdf.com"
-      gn.apiURL.should == "http://asdf.com"
-      gn.apiURL = apiurl
+      api_url = gn.api_endpoint_url
+      gn.api_endpoint_url = "http://asdf.com"
+      gn.api_endpoint_url.should == "http://asdf.com"
+      gn.api_endpoint_url = api_url
     end
   end
 
